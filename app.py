021b150aebbda67e5a610928113aebeb600b8e73
unknown <https://github.com/dpavankumar29/DevOps>
2026-04-15 21:30:08 +0530

    Version 1 - Initial CI/CD app

---
+++ app.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+PORT = 5500
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello from CI/CD V1 ")
+
+server = HTTPServer(("0.0.0.0", PORT), Handler)
+print(f"Server running on port {PORT}")
+server.serve_forever()
