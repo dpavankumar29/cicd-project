2695761730e362dc2530c73ca7eb5b2f43508a4f
unknown <https://github.com/dpavankumar29/DevOps>
2026-04-16 00:27:04 +0530

    Updated to V2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"CI CD Versioning 2... ")
+        self.wfile.write(b"Hello from CI/CD V1 ")
 
 server = HTTPServer(("0.0.0.0", PORT), Handler)
 print(f"Server running on port {PORT}")
